910b59b3c0c29053c9301bdb2a65e4aa430cc483
Milin Joshi <milincjoshi@gmail.com>
2017-04-02 20:38:18 -0700

    Check_Permutation

---
+++ CH_2/Check_Permutation_2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Check_Permutation_2{
+	
+
+	/*
+	with this program we check 
+	how many times each character occurs in each String
+	if the count does not match return false
+	Thus this program checks if each character occurs same number of times in each string
+	*/
+
+	public static boolean check_permutation(String str1, String str2){
+		HashMap<Character, Integer> hash_map = new HashMap<Character, Integer>();
+		if(str1.length() != str2.length()){
+			return false;
+		}
+
+		for(int i=0; i<str1.length();i++){
+			if(hash_map.containsKey(str1.charAt(i))){
+				hash_map.put(str1.charAt(i),hash_map.get(str1.charAt(i))+1);
+			}
+			else{
+				hash_map.put(str1.charAt(i),1);
+			}
+		}
+
+		for(int i=0; i<str2.length();i++){
+			if(!hash_map.containsKey(str2.charAt(i))){
+				return false;
+			}
+
+			int val = hash_map.get(str2.charAt(i));
+			val--;
+			if(val<0){
+				return false;
+			}
+
+			hash_map.put(str2.charAt(i), val);
+		}
+
+		return true;
+
+	}
+	public static void main(String[] args){
+
+		String x = "milin";
+		String y = "ilinm";
+
+		System.out.println(check_permutation(x,y));
+	}
+}
